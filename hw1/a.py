c5262ba9c35fb385b4cd94106275dc9227359ba5
yxu49 <yxu49@ucsc.edu>
2018-04-26 01:51:05 -0700

    p3

---
+++ hw1/a.py
@@ -0,0 +1,16 @@
+import math
+x=1/4
+y=3/4
+a= -x*math.log2(x)-y*math.log2(y)
+print(a)
+z=1/4
+w=3/4
+b= -z*math.log2(z)-w*math.log2(w)
+print(b)
+c=0.979
+d=0.985-c
+print(c)
+print(d)
+
+e=0.985-0.857
+print(e)
